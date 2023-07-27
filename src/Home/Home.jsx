616c56c4e720af04517a02cda6d574ba23690454
saadhzubairi <saadhzubairi@outlook.com>
2023-07-27 20:12:21 +0500

    initial commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./home.css"
+
+function Home(props) {
+    return (
+        <div>
+            <div className="backdrop"></div>
+            <div className="formPane">
+                
+            </div>
+        </div>
+    )
+}
+export default Home
